cfbeebf4e0a56613f623125f96fe421344605d49
parasmamgain <parasmamgain@google.com>
2023-09-12 08:55:57 +0000

    update helper scripts to cretae SA
    Change-Id: I0910214e89c67565dbf384e1e40ab1821cc7a6e0

--- helper-scripts/2.createserviceaccount.sh
+++ helper-scripts/2.createserviceaccount.sh
@@ -4,27 +4,39 @@
 set -e
 
 export HOST_PROJECT_ID=""
+#SA_PROJECT_ID is the GCP project where the service account will be created
+export SA_PROJECT_ID=""
 export SERVICE_PROJECT_ID=""
 export USER_PROJECT_ID=""
 export FOLDER_ID=""
-export USER_EMAIL_ID=""
-export SA_NAME="iac-sa"
+#IMPERONSATING_MEMBER will be of format "user:xyz@...com","group:xyz@...com" or "serviceAccount:xyz@...com"
+export IMPERONSATING_MEMBER=""
+export SA_NAME=""
 
-#gcloud config set project $HOST_PROJECT_ID
+gcloud config set project $SA_PROJECT_ID
 echo "============ Creating Service Account for the service account                 ============"
 
 #Create a service account in the host project to which the permission will be assigned
 gcloud iam service-accounts create $SA_NAME \
     --description="Service Account to be Used for creating GCP resources" \
     --display-name=$SA_NAME \
-    --project=$HOST_PROJECT_ID
+    --project=$SA_PROJECT_ID
+
+echo "=========================================================================================="
+
+echo "============ Setting Up storage admin Permission for the service account      ============"
+
+gcloud projects add-iam-policy-binding $SA_PROJECT_ID \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/storage.objectAdmin" \
+    --project=$SA_PROJECT_ID
 
 echo "=========================================================================================="
 
 echo "============ Setting Up XpnHost admin Permission for the service account      ============"
 
 gcloud resource-manager folders add-iam-policy-binding $FOLDER_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role='roles/compute.xpnAdmin'
 
 echo "=========================================================================================="
@@ -32,71 +44,76 @@ echo "==========================================================================
 echo "============ Setting Up Permission for the service account in Host Project ==============="
 
 gcloud projects add-iam-policy-binding $HOST_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
-    --role="roles/compute.networkAdmin"
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/compute.networkAdmin" \
+    --project=$HOST_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $HOST_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
-    --role="roles/iam.serviceAccountAdmin"
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/iam.serviceAccountAdmin" \
+    --project=$HOST_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $HOST_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
-    --role="roles/serviceusage.serviceUsageAdmin"
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/serviceusage.serviceUsageAdmin" \
+    --project=$HOST_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $HOST_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
-    --role="roles/resourcemanager.projectIamAdmin"
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/resourcemanager.projectIamAdmin" \
+    --project=$HOST_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $HOST_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/compute.securityAdmin" \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com"  \
+    --project=$HOST_PROJECT_ID
 
 # Following permission are assigned to the User who can then impersonate this service account
 
 gcloud iam service-accounts add-iam-policy-binding \
-    $SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com \
-    --member="user:$USER_EMAIL_ID" \
+    $SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com \
+    --member="$IMPERONSATING_MEMBER" \
     --role="roles/iam.serviceAccountUser" \
-    --project=$HOST_PROJECT_ID
+    --project=$SA_PROJECT_ID
 
 gcloud iam service-accounts add-iam-policy-binding \
-    $SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com \
-    --member="user:$USER_EMAIL_ID" \
+    $SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com \
+    --member="$IMPERONSATING_MEMBER" \
     --role="roles/iam.serviceAccountTokenCreator" \
-    --project=$HOST_PROJECT_ID
+    --project=$SA_PROJECT_ID
 
 echo "=========================================================================================="
 
 echo "============ Setting Up Permission for the service account in Service Project ============"
 
 gcloud projects add-iam-policy-binding $SERVICE_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/cloudsql.admin" \
     --project=$SERVICE_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $SERVICE_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/compute.instanceAdmin" \
     --project=$SERVICE_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $SERVICE_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/iam.serviceAccountAdmin" \
     --project=$SERVICE_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $SERVICE_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/iam.serviceAccountUser" \
     --project=$SERVICE_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $SERVICE_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/serviceusage.serviceUsageAdmin" \
     --project=$SERVICE_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $SERVICE_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/resourcemanager.projectIamAdmin" \
     --project=$SERVICE_PROJECT_ID
 
@@ -105,41 +122,39 @@ echo "==========================================================================
 echo "============ Setting Up Permission for the service account in User Project ==============="
 
 gcloud projects add-iam-policy-binding $USER_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/compute.networkAdmin" \
     --project=$USER_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $USER_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/compute.securityAdmin" \
     --project=$USER_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $USER_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/compute.instanceAdmin" \
     --project=$USER_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $USER_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/iam.serviceAccountAdmin" \
     --project=$USER_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $USER_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/iam.serviceAccountUser" \
     --project=$USER_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $USER_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/serviceusage.serviceUsageAdmin" \
     --project=$USER_PROJECT_ID
 
 gcloud projects add-iam-policy-binding $USER_PROJECT_ID \
-    --member="serviceAccount:$SA_NAME@$HOST_PROJECT_ID.iam.gserviceaccount.com" \
+    --member="serviceAccount:$SA_NAME@$SA_PROJECT_ID.iam.gserviceaccount.com" \
     --role="roles/resourcemanager.projectIamAdmin" \
     --project=$USER_PROJECT_ID
 
 echo "=========================================================================================="
 
-#iac-sa-test@pm-singleproject-20.iam.gserviceaccount.com
-#iac-sa-test1@pm-singleproject-20.iam.gserviceaccount.com
